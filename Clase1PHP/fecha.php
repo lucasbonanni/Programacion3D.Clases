d5f2979831e77e84faec447bbc8b7be8c7e7c29e
danag <le_bonanni@yahoo.com.ar>
2015-08-17 11:59:02 -0300

    Primer commit del respositorio

---
+++ Clase1PHP/fecha.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Fechas</title>
+</head>
+<body>
+	<?php
+	date_default_timezone_set('America/Argentina/Buenos_Aires');
+	echo date("d")."<br>";
+	echo date("l")."<br>";
+	echo date("D")."<br>";
+	echo date("j")."<br>";
+	echo date("c")."<br>";
+	echo date("r")."<br>";
+
+	?>
+</body>
+</html>
---
+++ Clase1PHP/suma.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title>Suma</title>
+</head>
+<body>
+	<?php
+	echo "<h1>Pagina de suma<h1>";
+	$num1 =32;
+	$num2 =12;
+	$resultado = $num1 + $num2;
+		echo "$resultado";
+		echo "<h1>".$resultado."<h1>";
+		echo '$resultado';
+	?>
+
+</body>
+</html>
